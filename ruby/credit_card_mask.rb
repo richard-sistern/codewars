e7077f8e8cfcb88c5b1fbf3c6a5503765ba8a207
Richard Sistern <richard.sistern@nnuh.nhs.uk>
2021-02-09 15:48:06 +0000

    Added Kata Credit Card Mask

---
+++ ruby/credit_card_mask.rb
@@ -0,0 +1,10 @@
+# Credit Card Mask
+
+def maskify(cc)
+  # your beautiful code goes here
+end
+
+maskify('4556364607935616') # should return '############5616'
+maskify('64607935616')      # should return '#######5616'
+maskify('1')                # should return '1'
+maskify('')                 # should return ''
