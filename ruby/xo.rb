a12c0365d332c3a73cc1bbf7980ba587362e4c40
Richard Sistern <richard.sistern@nnuh.nhs.uk>
2021-02-09 14:28:45 +0000

    Exes and Ohs Kata finished

---
+++ ruby/xo.rb
@@ -0,0 +1,13 @@
+# Exes and Ohs
+def XO(str)
+  x_count = str.scan(/[Xx]/).count
+  o_count = str.scan(/[Oo]/).count
+
+  x_count == o_count ? true : false
+end
+
+puts XO("ooxx") # true
+puts XO("xooxx") # false
+puts XO("ooxXm") # true
+puts XO("zpzpzpp") #true // when no 'x' and 'o' is present should return true
+puts XO("zzoo") # false
